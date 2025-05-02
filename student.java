4167a92b5347d5dce43268908020ed5e33c8bdf0
ShreyaUdupi05 <shreyakunder2@gmail.com>
2025-05-02 11:24:21 +0530

    first commit

---
+++ student.java
@@ -0,0 +1,9 @@
+public class student {
+    public static void main(String[] args) {
+        System.out.println("Student Registration Form");
+        System.out.println("Please enter your details below:");
+        System.out.println("Name: John Doe");
+        System.out.println("Age: 20");  
+
+    }
+}
