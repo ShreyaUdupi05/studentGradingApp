21b7ddf4afae5e72fa29b03a790b87255f18a233
ShabariShetty14 <shabarishetty405@gmail.com>
2025-05-02 14:34:21 +0530

    Update student.java

--- student.java
+++ student.java
@@ -2,8 +2,8 @@ public class student {
     public static void main(String[] args) {
         System.out.println("Student Registration Form");
         System.out.println("Please enter your details below:");
-        System.out.println("Name: John Doe");
-        System.out.println("Age: 20");  
+        System.out.println("Name: Shabari");
+        System.out.println("Age: 21");  
 
     }
 }
